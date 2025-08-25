eba8fc03ac8661eb97eb4c91c8413eecae6ffcf4
fightinghawks18 <lepagetyler@hotmail.com>
2025-08-24 19:43:58 -0500

    Added metadata to hopefully prevent accidental av tripping Added WindowProperties to window manager window creation Removed creation of Window via public

--- engine_glue/src/sys_c/window_manager.cpp
+++ engine_glue/src/sys_c/window_manager.cpp
@@ -15,16 +15,18 @@ C_API_FUNC(SYS_C_API, WindowManager*, window_manager_create, void)
     return reinterpret_cast<WindowManager*>(winManager);
 }
 
-C_API_FUNC(SYS_C_API, Window *, window_manager_create_window, WindowManager* wm)
+C_API_FUNC(SYS_C_API, Window*, window_manager_create_window, WindowManager* wm, WindowProperties props)
 {
     auto winManager = reinterpret_cast<lumi::sys::WindowManager*>(wm);
-    auto win = winManager->CreateWindow({
-        "Test Window",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        800,
-        600
-    });
+    
+    lumi::sys::WindowProperties winProps{};
+    winProps.title = props.title;
+    winProps.w = props.w;
+    winProps.h = props.h;
+    winProps.x = props.x;
+    winProps.y = props.y;
+
+    auto win = winManager->CreateWindow(winProps);
     return reinterpret_cast<Window*>(win.get());
 }
 
